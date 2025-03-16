1c220c113bd41cec75b5886af7683ac7a6073e0c
Aditya Prasetyo <adty404@gmail.com>
2025-03-16 17:42:38 +0700

    user service : Controller bagian 2

--- user-service/controllers/registry.go
+++ user-service/controllers/registry.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	controllers "user-service/controllers/user"
+	"user-service/services"
+)
+
+type Registry struct {
+	service services.IServicesRegistry
+}
+
+type IControllerRegistry interface {
+	GetUserController() controllers.IUserController
+}
+
+func NewControllerRegistry(service services.IServicesRegistry) IControllerRegistry {
+	return &Registry{service: service}
+}
+
+func (r *Registry) GetUserController() controllers.IUserController {
+	return controllers.NewUserController(r.service)
+}
